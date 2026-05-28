d4bc524834a1136911085130d079c63fc0682b2f
Bram Van der Sype <bram.vandersype@gmail.com>
2026-05-28 14:11:33 +0200

    Configure patchlevel with proper dependencies

--- src/Community/Infrastructure/CommunityContext.php
+++ src/Community/Infrastructure/CommunityContext.php
@@ -6,8 +6,30 @@ namespace CokidoPlanner\Community\Infrastructure;
 
 use Brammm\Smart\Context;
 use Brammm\Smart\Psr7\DefaultResponses;
+use Crell\EnvMapper\EnvMapper;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Tools\DsnParser;
 use Override;
+use Patchlevel\EventSourcing\Metadata\AggregateRoot\AggregateRootRegistry;
+use Patchlevel\EventSourcing\Metadata\AggregateRoot\AttributeAggregateRootRegistryFactory;
+use Patchlevel\EventSourcing\Repository\DefaultRepositoryManager;
+use Patchlevel\EventSourcing\Repository\RepositoryManager;
+use Patchlevel\EventSourcing\Serializer\DefaultEventSerializer;
+use Patchlevel\EventSourcing\Serializer\EventSerializer;
+use Patchlevel\EventSourcing\Store\DoctrineDbalStore;
+use Patchlevel\EventSourcing\Store\Store;
+use Patchlevel\EventSourcing\Subscription\Engine\DefaultSubscriptionEngine;
+use Patchlevel\EventSourcing\Subscription\Engine\SubscriptionEngine;
+use Patchlevel\EventSourcing\Subscription\Repository\RunSubscriptionEngineRepositoryManager;
+use Patchlevel\EventSourcing\Subscription\Store\DoctrineSubscriptionStore;
+use Patchlevel\EventSourcing\Subscription\Store\SubscriptionStore;
+use Patchlevel\EventSourcing\Subscription\Subscriber\MetadataSubscriberAccessorRepository;
+use Patchlevel\EventSourcing\Subscription\Subscriber\SubscriberAccessorRepository;
 use Slim\App;
+
+use function DI\factory;
+use function DI\get;
 
 final class CommunityContext implements Context
 {
@@ -20,6 +42,41 @@ final class CommunityContext implements Context
     #[Override]
     public function dependencies(): array
     {
-        return [];
+        return [
+            ConnectionsEnv::class => static fn() => new EnvMapper()->map(ConnectionsEnv::class),
+            'connection.events' =>
+                static fn(ConnectionsEnv $env) => DriverManager::getConnection(new DsnParser()->parse($env->eventsDsn)),
+            'connection.projections' =>
+                static fn(ConnectionsEnv $env) => DriverManager::getConnection(new DsnParser()->parse($env->projectionsDsn)),
+            EventSerializer::class => DefaultEventSerializer::createFromPaths([
+                __DIR__ . '/../Domain',
+            ]),
+            AggregateRootRegistry::class => static fn() => new AttributeAggregateRootRegistryFactory()->create([
+                __DIR__ . '/../Domain',
+            ]),
+            Store::class => factory(static fn(
+                Connection $connection,
+                EventSerializer $serializer,
+            ) => new DoctrineDbalStore($connection, $serializer))
+                ->parameter('connection', get('connection.events')),
+            SubscriberAccessorRepository::class => static fn() => new MetadataSubscriberAccessorRepository([]),
+            SubscriptionStore::class => factory(
+                static fn(Connection $connection) => new DoctrineSubscriptionStore($connection),
+            )
+                ->parameter('connection', get('connection.events')),
+            SubscriptionEngine::class => static fn(
+                Store $eventStore,
+                SubscriptionStore $subscriptionStore,
+                SubscriberAccessorRepository $subscriberAccessorRepository,
+            ) => new DefaultSubscriptionEngine($eventStore, $subscriptionStore, $subscriberAccessorRepository),
+            RepositoryManager::class => static fn(
+                AggregateRootRegistry $aggregateRootRegistry,
+                Store $eventStore,
+                SubscriptionEngine $engine,
+            ) => new RunSubscriptionEngineRepositoryManager(
+                new DefaultRepositoryManager($aggregateRootRegistry, $eventStore),
+                $engine,
+            ),
+        ];
     }
 }
---
+++ src/Community/Infrastructure/ConnectionsEnv.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CokidoPlanner\Community\Infrastructure;
+
+final readonly class ConnectionsEnv
+{
+    public string $eventsDsn;
+
+    public string $projectionsDsn;
+}
